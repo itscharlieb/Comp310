dfb75e1c71e4683cdd100facec846080d16b16ee
Charles Bloomfield <charles.bloomfield@mail.mcgill.ca>
2015-02-16 23:41:14 -0500

    Renamed Ass1 to "shell"

---
+++ shell/commandlist.c
@@ -0,0 +1,60 @@
+/*
+Author: Charles Bloomfield, 260520615
+For Comp 310, Assignment 1
+*/
+
+#include <commandlist.h>
+
+void initializeCommandList(commandList* commandList){
+	commandList->head = -1;
+}
+
+char firstChar(char** cpp){
+	return **cpp;
+}
+
+//adds a command to the history, removing and old command if necessary
+void addCommandToList(commandList* commandList, char* command[]){
+	commandList->head ++;
+	commandList->numCommandsAdded ++;
+	//loop back
+	if(commandList->head == 10){
+		commandList->head = 0;
+	}
+	commandList->commands[commandList->head] = command;
+}
+
+//returns a reference to the first command starting with character c
+char** getCommand(commandList* commandList, char c){
+	int i;
+	int n = commandList->numCommandsAdded;
+	// printf("Request getCommand starting with %c.\n", c);
+	//printList(commandList);
+	int head = commandList->head;
+
+	//do circular search if more than n commands have been added
+	if(n > 10){
+		for(i = head; i >= (head - 10); i--){
+			if(firstChar(commandList->commands[i % 10]) == c){
+				return commandList->commands[i % 10];
+			}
+		}
+	}
+
+	//search starting at the head back to the beginning with no circulating.
+	else{
+		for(i = head; i >= 0; i--){
+			if(firstChar(commandList->commands[i]) == c){
+				return commandList->commands[i];
+			}
+		}
+	}
+
+	printf("There is no command starting with character '%c' in your history.\n", c);
+	return NULL;
+}
+
+//returns a reference to the most recently added command
+char** getHeadCommand(commandList* commandList){
+	return commandList->commands[commandList->head];
+}
